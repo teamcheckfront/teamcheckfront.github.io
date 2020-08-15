3af162b996fb968df785f3ffa18d7447c23459d3
sakandola <sakandola@hotmail.com>
2020-08-15 14:11:50 -0700

    Updates

---
+++ precache-manifest.1546444dc8647fe3eb5a11c216532c8f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8e7aae32f5ee53e1bca104313e92ce0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "beab7dff5dab6bf035f6",
+    "url": "/static/css/2.3db8585d.chunk.css"
+  },
+  {
+    "revision": "c5d6274e42fcf1818210",
+    "url": "/static/css/main.2e4c20bb.chunk.css"
+  },
+  {
+    "revision": "beab7dff5dab6bf035f6",
+    "url": "/static/js/2.f752ff11.chunk.js"
+  },
+  {
+    "revision": "07efe609b3855f54336ad553ac99d6be",
+    "url": "/static/js/2.f752ff11.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c5d6274e42fcf1818210",
+    "url": "/static/js/main.6615114e.chunk.js"
+  },
+  {
+    "revision": "80d2b490fa523d5433dc",
+    "url": "/static/js/runtime-main.51ce40a3.js"
+  },
+  {
+    "revision": "a92663071f7ed051561b3a0e8568baa4",
+    "url": "/static/media/boat.a9266307.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.81e02d5a2563e8ba7656b4d4478ca051.js"
+  "/precache-manifest.1546444dc8647fe3eb5a11c216532c8f.js"
 );
 
 self.addEventListener('message', (event) => {
